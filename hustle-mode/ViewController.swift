a68b3c97a2b6cffe06079447227b2f1ce0a842d9
Derek Freer <derek.freer@kalidy.com>
2017-09-29 16:07:02 -0500

    first iOS app

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var darkBlueBg: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    @IBOutlet weak var onMode: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var reset: UIButton!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onPressPowerButton(_ sender: Any) {
+        powerButton.isHidden = true
+        darkBlueBg.isHidden = true
+        onMode.isHidden = false
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 100, width: 375, height: 667)
+        }) { (finished) in
+            self.hustleLabel.isHidden = false
+            self.statusLabel.isHidden = false
+            self.reset.isHidden = false
+        }
     }
-
-
+    
+    @IBAction func onClickReset(_ sender: Any) {
+        
+        reset.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 400, width: 375, height: 667)
+        }) { (finished) in
+            self.onMode.isHidden = true
+            self.hustleLabel.isHidden = true
+            self.statusLabel.isHidden = true
+            
+            self.powerButton.isHidden = false
+            self.darkBlueBg.isHidden = false
+        }
+    }
+    
 }
-
