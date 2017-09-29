5eb79369ebb1270619dd77a46d2cfc9976518fdb
Derek Freer <derek.freer@kalidy.com>
2017-09-29 09:24:49 -0500

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Derek Freer on 9/29/17.
+//  Copyright © 2017 Derek Freer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
